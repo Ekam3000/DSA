c63e6a87425ff4f9eee8d297340ced0425366f81
ngarg2k2 <garg2002nipun@gmail.com>
2022-01-08 11:53:15 +0530

    added more conversions

--- Revision/Functn/conversion.cpp
+++ Revision/Functn/conversion.cpp
@@ -79,9 +79,34 @@ void decimalToHexa(int n)
     }
     cout << endl;
 }
+void binaryToDecimal(int n)
+{
+    int ans = 0;
+    for (int x = 1; n > 0; x = x * 2)
+    {
+        ans = ans + x * (n % 10);
+        n = n / 10;
+    }
+    cout << "Decimal " << ans << endl;
+}
+void octaToDecimal(int n)
+{
+    int ans = 0;
+    for (int x = 1; n > 0; x = x * 8)
+    {
+        ans = ans + x * (n % 10);
+        n = n / 10;
+    }
+    cout << "Decimal " << ans << endl;
+}
+void hexaToDecimal(int n)
+{
+    // Pending
+    //  cout <<"Decimal "<< ans << endl;
+}
 int main()
 {
-    cout << "1:-decimalToBinary \n2:-decimalToOcta \n3:-decimalToHexa" << endl;
+    cout << "1:-decimalToBinary \n2:-decimalToOcta \n3:-decimalToHexa\n4:-binaryToDecimal\n5:-octaToDecimal\n6:-hexaToDecimal" << endl;
     int choice;
     cin >> choice;
     switch (choice)
@@ -104,6 +129,24 @@ int main()
         cin >> n;
         cout << "Decimal " << n << endl;
         decimalToHexa(n);
+        break;
+    case 4:
+        cout << "Enter a binary number" << endl;
+        cin >> n;
+        cout << "Binary " << n << endl;
+        binaryToDecimal(n);
+        break;
+    case 5:
+        cout << "Enter a octa number" << endl;
+        cin >> n;
+        cout << "Octa " << n << endl;
+        octaToDecimal(n);
+        break;
+    case 6:
+        cout << "Enter a hexa number" << endl;
+        cin >> n;
+        cout << "Hexa " << n << endl;
+        hexaToDecimal(n);
         break;
 
     default:
